0ef9a6c356ac7e38cb773bc458fab28f6d74387f
gamerhurb.com <gamerhurb.com@gmail.com>
2024-12-05 16:14:47 -0300

    Atualização do projeto, correções e melhorias

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  // ou defina a URL base corretamente caso não esteja na raiz
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
