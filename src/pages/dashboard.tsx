390d0d14ffe3e9bd822d3d091781059b2b7a4d2a
gamerhurb.com <gamerhurb.com@gmail.com>
2024-12-05 23:35:59 -0300

    Criada página Dashboard em /pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Dashboard } from '../components/Dashboard'; // Ajuste o caminho conforme necessário
+
+const DashboardPage = () => {
+    return <Dashboard />;
+};
+
+export default DashboardPage;
